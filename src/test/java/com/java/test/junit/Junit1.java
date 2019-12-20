f2b7b437f0e78c9ecae2eb327fb031d1d0293601
Piyush.Prasad <Piyush.Prasad@CORP.AMAZONWORKSPACES.COM>
2019-12-20 13:52:31 +0530

    Added Files

---
+++ src/test/java/com/java/test/junit/Junit1.java
@@ -0,0 +1,13 @@
+package com.java.test.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Junit1 {
+
+	@Test
+	public void testMethod() {
+		assertEquals(10, 10);
+	}
+}
